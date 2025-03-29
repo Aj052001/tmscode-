5631eadffdcc19af0f063c35fa9f4e0789ae7f4b
Ajay Singh <ajsingh052001@gmail.com>
2025-03-29 12:26:23 +0530

    add paragraph

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-   <h1>Tms computer classes</h1>
+  
+   <>
+
+<h1>Tms computer classes</h1>
+
+ <p>hello this is computer classes</p>
+   </>
   );
 }
 
