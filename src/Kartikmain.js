5a82a88f39c39417c8da2b6e0f1ca2d3a6917aad
kartikey-alt <kartikeykct01@gmail.com>
2025-03-29 14:20:59 +0530

    add main page

---
+++ src/Kartikmain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Kartikmain.css'
+
+function Kartikmain() {
+  return (
+    <div className='mainpage'>
+      <h1> KARTIK</h1>
+    </div>
+  )
+}
+
+export default Kartikmain
